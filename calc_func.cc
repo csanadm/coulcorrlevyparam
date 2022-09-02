f5635c354534aa527311673ef74ea784b4729be0
csanadm <38218165+csanadm@users.noreply.github.com>
2022-09-02 12:00:53 +0200

    Correlation function calculation with Coulomb

--- calc_func.cc
+++ calc_func.cc
@@ -1,12 +1,23 @@
+#include <cstdlib>
 #include <iostream>
 #include "coulcorr_param.h"
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
-  double param[3] = {1,5,1.1}; //lambda, R, alpha
-  for(double q=0; q<0.2; q+=0.002)
+  if(argc!=4)
   {
-    cout << q << "\t" << coulcorr_param(&q,&param[0]) << endl;
+    cerr << "Usage: " << argv[0] << " <lambda> <R> <alpha>" << endl;
+    return 1;
+  }
+  double param[3] = {atof(argv[1]),atof(argv[2]),atof(argv[3])}; //lambda, R, alpha
+  for(double q=0.001; q<0.2; q+=0.001)
+  {
+    double lambda = param[0];
+    double R = param[1];
+    double alpha = param[2];
+    double coulcorr = coulcorr_param(&q,&param[0]);
+    double c2 = 1+exp(-pow(q*R/HBARC,alpha));
+    cout << q << "\t" << 1-lambda+lambda*coulcorr*c2 << "\t" << coulcorr << endl;
   }
 }
